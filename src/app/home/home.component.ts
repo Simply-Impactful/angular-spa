084b16f1e6b878a0487fddc7636ec921b3ddb089
Hendricks, Emily <emily.hendricks@fmr.com>
2018-08-23 17:28:53 -0400

    home page enhancements. banner and action buttons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,12 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  userscore: number = 45;
+  userscore: number = 0;
   username: string = 'Jack';
+  actionPoints: number = 0;
+  isUnplug: boolean = true;
+  name: string="";
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  // Add the number of points assigned to each action
+  add(name: string){
+    if(name=="faucet"){
+      this.actionPoints=5;
+    }
+    if(name=="lights"){
+      this.actionPoints=5;
+    }
+    if(name=="unplug"){
+      this.actionPoints=7;
+    }
+    // = this.actionPoint+1;
+    this.userscore = this.userscore+this.actionPoints;
+  }
 }
