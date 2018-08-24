a346a3a6102e5c278cd7877f034f94cf4c671ca2
Hendricks, Emily <emily.hendricks@fmr.com>
2018-08-24 19:17:49 -0400

    adding the new CIS logos. placed upload a picture next to create button. and home page css/html tweaks for flags. need to remove static locations of flags

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,13 +4,16 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
-})
+})//
 export class HomeComponent implements OnInit {
   userscore: number = 0;
   username: string = 'Jack';
   actionPoints: number = 0;
-  isUnplug: boolean = true;
+  faucetPoints: number = 5;
+  unplugPoints: number = 7;
+  lightsPoints: number = 5;
   name: string="";
+
 
   constructor() { }
 
@@ -18,15 +21,16 @@ export class HomeComponent implements OnInit {
   }
 
   // Add the number of points assigned to each action
+  // should make the point allocation be a method call and pass the action type to return the points
   add(name: string){
     if(name=="faucet"){
-      this.actionPoints=5;
+      this.actionPoints=this.faucetPoints;
     }
     if(name=="lights"){
-      this.actionPoints=5;
+      this.actionPoints=this.lightsPoints;
     }
     if(name=="unplug"){
-      this.actionPoints=7;
+      this.actionPoints=this.unplugPoints;
     }
     // = this.actionPoint+1;
     this.userscore = this.userscore+this.actionPoints;
