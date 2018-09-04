df4c288bc2d4d1d0be28263ec9e5ef9c8dd426a2
Hendricks, Emily <emily.hendricks@fmr.com>
2018-09-04 14:53:40 -0400

    login service tweak

--- src/app/services/log-in.service.ts
+++ src/app/services/log-in.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { User } from '../model/User';
-import { Observable } from 'rxjs';
-import { map, catchError , tap } from 'rxjs/operators';
 import { LoggedInCallback, CognitoCallback, CognitoUtil } from './cognito.service';
 import { AuthenticationDetails, CognitoUser, CognitoUserSession } from "amazon-cognito-identity-js";
 import * as AWS from "aws-sdk/global";
@@ -73,39 +71,8 @@ export class LogInService {
 
   private onLoginError = (callback: CognitoCallback, err) => {
     callback.cognitoCallback(err.message, null);
-  }
 
-  login(username: string, password: string): Observable<User>{
-    let requestBody = { "username" : username, "password": password};
-    return this.http.get('../../../mocks/login.json')
-    .pipe(
-      map(user => {
-          if(user.isLoggedIn) {
-            // once they're logged in, we'll route to the landing page
-          console.log("logged In successfully");
-          }
-          else{
-            console.log("User was not logged in successfully");
-          }
-          return user;
-      })
-    )
   }
-    // this doesn't exist yet, but the controller will route to the node.js login API call to validate credentials
-  /** 
-   return this.http.post<User>(this.apiEndpoint + 'login',requestBody) 
-  .pipe(
-      map(user => {
-          if(user.isLoggedIn) {
-            // once they're logged in, we'll route to the landing page
-          console.log("logged In successfully");
-          }
-          else{
-            console.log("User was not logged in successfully");
-          }
-          return user;
-      })
-    )*/
 
     isAuthenticated(callback: LoggedInCallback){
       return false;
