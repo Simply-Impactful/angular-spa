f7e0560982f08d59448cd2ba60f2c4234ad03eb3
DeepaManur <Deepagoud@gmail.com>
2018-09-25 11:47:07 -0400

    Admin Actions

--- src/app/admin-access-actions/admin-access-actions.component.ts
+++ src/app/admin-access-actions/admin-access-actions.component.ts
@@ -1,20 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { Route, Router } from '@angular/router';
+import {MatTableDataSource, MatButton} from '@angular/material';
+
+
+export interface ActionData {
+  ACTION: string;
+  POINTS: number;
+  LIMIT: number;
+  FREQUENCY: string;
+  FACT: String;
+  IMAGE: String;
+  ICON: String;
+    // Remove: CheckboxRequiredValidator;
+}
+
+const ACTION_DATA: ActionData[] = [
+
+  {ACTION: 'Skipped the show', POINTS: 1, LIMIT: 2, FREQUENCY: 'Per Day', FACT: 'Americans use 500 straw per day',
+  IMAGE: 'show', ICON: 'straw'},
+  {ACTION: 'Skipped the show', POINTS: 1, LIMIT: 2, FREQUENCY: 'Per Day', FACT: 'Americans use 500 straw per day',
+  IMAGE: 'show', ICON: 'straw'},
+  {ACTION: 'Skipped the show', POINTS: 1, LIMIT: 2, FREQUENCY: 'Per Day', FACT: 'Americans use 500 straw per day',
+  IMAGE: 'show', ICON: 'straw'},
+  {ACTION: 'Skipped the show', POINTS: 1, LIMIT: 2, FREQUENCY: 'Per Day', FACT: 'Americans use 500 straw per day',
+  IMAGE: 'show', ICON: 'straw'},
+  {ACTION: 'Skipped the show', POINTS: 1, LIMIT: 2, FREQUENCY: 'Per Day', FACT: 'Americans use 500 straw per day',
+  IMAGE: 'show', ICON: 'straw'},
+  {ACTION: 'Skipped the show', POINTS: 1, LIMIT: 2, FREQUENCY: 'Per Day', FACT: 'Americans use 500 straw per day',
+  IMAGE: 'show', ICON: 'straw'},
+];
 
 @Component({
   selector: 'app-admin-access-actions',
   templateUrl: './admin-access-actions.component.html',
   styleUrls: ['./admin-access-actions.component.scss']
 })
-export class AdminAccessActionsComponent implements OnInit {
 
-  constructor(public appComp: AppComponent) { }
+export class AdminAccessActionsComponent implements OnInit {
+  displayedColumns: string[] = ['ACTION', 'POINTS', 'LIMIT', 'FREQUENCY', 'FACT', 'IMAGE', 'ICON'];
+  columnsToDisplay: string[] = this.displayedColumns.slice();
+  data: ActionData[] = ACTION_DATA;
+
+   constructor(public appComp: AppComponent) { }
 
   ngOnInit() {
     this.appComp.setAdmin();
   }
 
-  save() {}
-
 }
+
+
