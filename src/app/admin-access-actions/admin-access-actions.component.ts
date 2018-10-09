e4e06bf26c4c569ae3fabbff61fc4373c19f375d
DeepaManur <Deepagoud@gmail.com>
2018-10-09 12:58:22 -0400

    carbon points and Assignment URL create and edit action

--- src/app/admin-access-actions/admin-access-actions.component.ts
+++ src/app/admin-access-actions/admin-access-actions.component.ts
@@ -49,8 +49,8 @@ export class AdminAccessActionsComponent implements OnInit, LoggedInCallback {
 
   edit() {
     const dialogRef = this.dialog.open(AdminActionDialogComponent, {
-      width: '700px',
-      height: '600px',
+      width: '650px',
+      height: '675px',
       data: this.actions[this.index]
     });
 
@@ -76,8 +76,8 @@ export class AdminAccessActionsComponent implements OnInit, LoggedInCallback {
 
   create() {
     const dialogRef = this.dialog.open(AdminActionDialogComponent, {
-      width: '700px',
-      height: '600px',
+      width: '650px',
+      height: '675px',
     });
 
     dialogRef.afterClosed().subscribe(result => {
