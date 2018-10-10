d8c9518ca6955990a45b4700372601265ce0ad45
Lucena, Maria <a570277@fmr.com>
2018-10-10 17:04:00 -0400

    removing file uploader, adding s3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,7 +9,6 @@ import { ActionComponent } from './action/action.component';
 import { ActionDialogComponent } from './action-dialog/action-dialog.component';
 import { AppComponent } from './app.component';
 import { AppTopNavComponent } from './app-top-nav/app-top-nav.component';
-import { AngularFileUploaderModule } from 'angular-file-uploader';
 import { CreateGroupComponent } from './create-group/create-group.component';
 import { CreateProfileComponent } from './create-profile/create-profile.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
@@ -18,7 +17,6 @@ import { LogInComponent } from './log-in/log-in.component';
 import { LandingComponent } from './landing/landing.component';
 import { MaterialModule } from './material.module';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import { UploadComponent } from './upload/upload.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 import { AwsUtil } from './services/aws.service';
@@ -40,13 +38,11 @@ describe('AppComponent', () => {
         HomeComponent,
         LogInComponent,
         LandingComponent,
-        UploadComponent,
         UserProfileComponent,
         ResetPasswordComponent
       ],
       imports: [
         [BrowserModule, FormsModule],
-        AngularFileUploaderModule,
         MaterialModule,
         RouterTestingModule,
         HttpClientTestingModule
