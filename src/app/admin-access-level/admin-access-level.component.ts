7518e299912c41a0c56bcb3416c54fa02ac96f78
DeepaManur <Deepagoud@gmail.com>
2018-10-18 20:08:26 -0400

    get Level Admin page

--- src/app/admin-access-level/admin-access-level.component.ts
+++ src/app/admin-access-level/admin-access-level.component.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Levels } from '../model/Levels';
+import { LogInService } from '../services/log-in.service';
+import { Parameters} from '../services/parameters';
+import { CognitoUtil, LoggedInCallback } from '../services/cognito.service';
+import { AWSError } from 'aws-sdk';
+import { LambdaInvocationService } from '../services/lambdaInvocation.service';
+import { Router } from '@angular/router';
 import { AppComponent } from '../app.component';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
+
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-admin-access-level',
@@ -8,38 +19,56 @@ import { AppComponent } from '../app.component';
 })
 export class AdminAccessLevelComponent implements OnInit {
 
+  levels: Levels[];
+  displayedColumns = ['pointsRange', 'status', 'statusGraphicUrl'];
+  dataSource;
+
   inputText;
   editField: string;
-  levelList: Array<any> = [
-    { id: 1, pointsRange: '0 - 100',  status: 'Elephant',
-    statusGraphicUrl: 'https://s3.amazonaws.com/simply-impactful-image-data/Levels/elephant2.svg' },
-    { id: 2, pointsRange: '100 - 200',  status: 'Lemur',
-    statusGraphicUrl: 'https://s3.amazonaws.com/simply-impactful-image-data/Levels/lemur.svg' },
-  ];
+  // levelList: Array<any> = [
+  //  { id: 1, pointsRange: '0 - 100',  status: 'Elephant',
+  //  statusGraphicUrl: 'https://s3.amazonaws.com/simply-impactful-image-data/Levels/elephant2.svg' },
+  //  { id: 2, pointsRange: '100 - 200',  status: 'Lemur',
+  //  statusGraphicUrl: 'https://s3.amazonaws.com/simply-impactful-image-data/Levels/lemur.svg' },
+  // ];
 
   awaitingLevelList: Array<any> = [];
 
-  constructor(private appComp: AppComponent) { }
+  constructor( public appComp: AppComponent, public lambdaService: LambdaInvocationService) { }
 
   ngOnInit() {
-    this.appComp.setAdmin();
+    this.lambdaService.listLevelData(this);
   }
+
+isLoggedIn(message: string, loggedIn: boolean): void {}
+  callbackWithParams(error: AWSError, result: any): void {
+    if (result) {
+      const response = JSON.parse(result);
+      this.levels = response.body;
+      console.log('response.body', response.body);
+      this.dataSource = new MatTableDataSource(this.levels);
+     } else {
+      console.log('error pulling the levels data' + error);
+    }
+  }
+  callbackWithParam(result: any): void {}
+
 
   saveNew() {}
 
   updateList(id: number, property: string, event: any) {
     const editField = event.target.textContent;
-    this.levelList[id][property] = editField;
+    this.levels[id][property] = editField;
   }
 
   remove(id: any) {
-    this.awaitingLevelList.push(this.levelList[id]);
-    this.levelList.splice(id, 1);
+    this.awaitingLevelList.push(this.levels[id]);
+    this.levels.splice(id, 1);
   }
 
   add() {
       const level = this.awaitingLevelList[0];
-      this.levelList.push(level);
+      this.levels.push(level);
       this.awaitingLevelList.splice(0, 1);
   }
 
