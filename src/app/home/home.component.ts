39ecfd68377e3f73f159c30bb83fe0a0b15301bf
VinuthnaReddyy <vinuthnaui@gmail.com>
2020-10-07 22:39:30 -0500

    Merging api-gateway changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Group } from '../model/Group';
 import { BehaviorSubject } from 'rxjs';
 import { User } from '../model/User';
 import { LogInService } from '../services/log-in.service';
-import { Parameters} from '../services/parameters';
+import { Parameters } from '../services/parameters';
 import { CognitoUtil, LoggedInCallback, Callback } from '../services/cognito.service';
 import { CreateProfileService } from '../services/create-profile.service';
 import { AWSError } from 'aws-sdk';
@@ -45,18 +45,18 @@ export class HomeComponent implements OnInit, LoggedInCallback, Callback {
     this.loginService.isAuthenticated(this);
   }
 
-   // LoggedInCallback interface
-   isLoggedIn(message: string, isLoggedIn: boolean) {
-     if (!isLoggedIn) {
-       this.router.navigate(['/login']);
-     } else {
+  // LoggedInCallback interface
+  isLoggedIn(message: string, isLoggedIn: boolean) {
+    if (!isLoggedIn) {
+      this.router.navigate(['/login']);
+    } else {
       // get All the actions to parse the assignments
       // response in callbackWithParams method
       this.lambdaService.listActions(this);
       // kick off the levels data
       this.levelsMapping.getAllData();
-     }
-   }
+    }
+  }
 
   // response of isAuthenticated method in login service
   callbackWithParam(result: any): void {
@@ -70,20 +70,20 @@ export class HomeComponent implements OnInit, LoggedInCallback, Callback {
       this.lambdaService.getUserActions(this, this.user);
 
     }
-   }
+  }
 
-   // Response of listActions API - logged in callback interface
+  // Response of listActions API - logged in callback interface
   callbackWithParams(error: AWSError, result: any) {
     if (result) {
       const response = JSON.parse(result);
       const listOfActions = response.body;
       const listOfActionsLength = listOfActions.length;
-        for ( let i = 0; i < listOfActionsLength; i++ ) {
-          if (listOfActions[i].assignmentUrl) {
-            // for rendering the assignment and its corresponding name on the UI
-            this.assignments.push(listOfActions[i].assignmentUrl);
-            this.actionNames.push(listOfActions[i].name);
-          }
+      for (let i = 0; i < listOfActionsLength; i++) {
+        if (listOfActions[i].assignmentUrl) {
+          // for rendering the assignment and its corresponding name on the UI
+          this.assignments.push(listOfActions[i].assignmentUrl);
+          this.actionNames.push(listOfActions[i].name);
+        }
       }
     }
   }
@@ -99,12 +99,12 @@ export class HomeComponent implements OnInit, LoggedInCallback, Callback {
         this.user.totalPoints = 0;
         this.getLevelsData();
       } else {
-          const userActionsLength = userActions.length;
-          // if the user has taken actions
-          if (userActionsLength > 0) {
-            for ( let i = 0; i < userActionsLength; i++ ) {
-              this.user.totalPoints = userActions[i].totalPoints;
-              this.getLevelsData();
+        const userActionsLength = userActions.length;
+        // if the user has taken actions
+        if (userActionsLength > 0) {
+          for (let i = 0; i < userActionsLength; i++) {
+            this.user.totalPoints = userActions[i].totalPoints;
+            this.getLevelsData();
           }
         }
       }
@@ -112,13 +112,13 @@ export class HomeComponent implements OnInit, LoggedInCallback, Callback {
     this.getLevelName(this.user.totalPoints);
   }
 
-   // for switching back and forth between actions page
-   navigate() {
-     this.isViewAll = true;
-     this.isHomePage = false;
-   }
-    // for switching back and forth between actions
-   backHome() {
+  // for switching back and forth between actions page
+  navigate() {
+    this.isViewAll = true;
+    this.isHomePage = false;
+  }
+  // for switching back and forth between actions
+  backHome() {
     this.isHomePage = true;
     this.isViewAll = false;
   }
@@ -135,20 +135,20 @@ export class HomeComponent implements OnInit, LoggedInCallback, Callback {
         this.user.level = this.levelsMapping.getUserLevel(this.user, response);
       }
     });
-   }
+  }
 
-  cognitoCallbackWithParam(result: any) {}
+  cognitoCallbackWithParam(result: any) { }
 
-  callback() {}
+  callback() { }
 
   getLevelName(points: Number) {
     const levels = Object.keys(LevelsEnum).slice(0, 10);
-    for ( let i=0; i < 10; i++ ) {
-        if ( this.user.totalPoints < Number( levels[i] )) {
-          this.currLevel = String( Object.values(LevelsEnum)[i] );
-          break;
-       };
-    };
-};
+    for (let i = 0; i < 10; i++) {
+      if (this.user.totalPoints < Number(levels[i])) {
+        this.currLevel = String(Object.values(LevelsEnum)[i]);
+        break;
+      }
+    }
+  }
 
 }
